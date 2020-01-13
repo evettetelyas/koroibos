a7218a5898e3f2a4c1013da0267b087d51295d38
evettetelyas <etelyas@gmail.com>
2020-01-13 15:09:45 -0700

    change db url to ENV variable

--- knexfile.js
+++ knexfile.js
@@ -28,7 +28,7 @@ module.exports = {
 
   production: {
 		client: 'pg',
-    connection: 'postgres://qoefmjwjkvxkoo:9442d4a2cf20b94e1ffcf0fe91a265cd89ce12165267e051fb929b169c91ac7c@ec2-3-220-86-239.compute-1.amazonaws.com:5432/dcfe875df8orm9',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
